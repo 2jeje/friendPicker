a1d7d999ad774a105cfd2a36bfe4f786ce42c1a8
jeje.e <jeje.e@kakaocorp.com>
2021-03-17 16:47:51 +0900

    recyclerview impl

---
+++ app/src/main/java/com/jeje/friendpicker/FriendPickerAdapter.kt
@@ -0,0 +1,42 @@
+package com.jeje.friendpicker
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FriendPickerAdapter(private var list: MutableList<TestData>) : RecyclerView.Adapter<FriendPickerAdapter.ListItemViewHolder>() {
+
+
+    inner class ListItemViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView!!) {
+
+        var data1Text: TextView = itemView!!.findViewById(R.id.data1Text)
+        var data2Text: TextView = itemView!!.findViewById(R.id.data2Text)
+        var data3Text: TextView = itemView!!.findViewById(R.id.data3Text)
+
+        // onBindViewHolder의 역할을 대신한다.
+        fun bind(data: TestData, position: Int) {
+            Log.d("ListAdapter", "===== ===== ===== ===== bind ===== ===== ===== =====")
+            Log.d("ListAdapter", data.getData1()+" "+data.getData2()+" "+data.getData3())
+            data1Text.text = data.getData1()
+            data2Text.text = data.getData2()
+            data3Text.text = data.getData3()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_data_list, parent, false)
+        return ListItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.count()
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        holder.bind(list[position], position)
+    }
+
+}
---
+++ app/src/main/java/com/jeje/friendpicker/FriendPickerFragment.kt
@@ -0,0 +1,99 @@
+package com.jeje.friendpicker
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_friend_picker.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FriendPickerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FriendPickerFragment : Fragment() {
+
+    // RecyclerView.adapter에 지정할 Adapter
+    private lateinit var pickerAdapter: FriendPickerAdapter
+
+    var dataList: ArrayList<TestData> = arrayListOf(
+        TestData("첫 번째 데이터1", "두 번째 데이터1", "세 번째 데이터1"),
+        TestData("첫 번째 데이터2", "두 번째 데이터2", "세 번째 데이터2"),
+        TestData("첫 번째 데이터3", "두 번째 데이터3", "세 번째 데이터3"),
+        TestData("첫 번째 데이터4", "두 번째 데이터4", "세 번째 데이터4"),
+        TestData("첫 번째 데이터5", "두 번째 데이터5", "세 번째 데이터5"),
+        TestData("첫 번째 데이터6", "두 번째 데이터6", "세 번째 데이터6"),
+        TestData("첫 번째 데이터7", "두 번째 데이터7", "세 번째 데이터7"),
+        TestData("첫 번째 데이터8", "두 번째 데이터8", "세 번째 데이터8"),
+        TestData("첫 번째 데이터9", "두 번째 데이터9", "세 번째 데이터9"),
+        TestData("첫 번째 데이터10", "두 번째 데이터10", "세 번째 데이터10"),
+        TestData("첫 번째 데이터11", "두 번째 데이터11", "세 번째 데이터11"),
+        TestData("첫 번째 데이터12", "두 번째 데이터12", "세 번째 데이터12"),
+        TestData("첫 번째 데이터13", "두 번째 데이터13", "세 번째 데이터13"),
+        TestData("첫 번째 데이터14", "두 번째 데이터14", "세 번째 데이터14"),
+        TestData("첫 번째 데이터15", "두 번째 데이터15", "세 번째 데이터15")
+    )
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friend_picker, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var list: ArrayList<TestData> = requireActivity().intent!!.extras!!.get("DataList") as ArrayList<TestData>
+        Log.e("FirstFragment", "Data List: ${list}")
+
+        // Fragment에서 전달받은 list를 넘기면서 ListAdapter 생성
+        pickerAdapter = FriendPickerAdapter(list)
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        // RecyclerView.adapter에 지정
+        recyclerView.adapter = pickerAdapter
+    }
+
+}
+
+class TestData(
+    private var data1: String? = null,
+    private var data2: String? = null,
+    private var data3: String? = null
+) {
+    fun getData1(): String? {
+        return data1
+    }
+
+    fun setData1(name: String) {
+        this.data1 = data1
+    }
+
+    fun getData2(): String? {
+        return data2
+    }
+
+    fun setData2(address: String) {
+        this.data2 = data2
+    }
+
+    fun getData3(): String? {
+        return data3
+    }
+
+    fun setData3(type: String) {
+        this.data3 = data3
+    }
+}
