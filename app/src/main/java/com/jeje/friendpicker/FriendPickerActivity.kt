40e5a824a59f1511d8d5d87010f3e50fafd2d5b8
jeje.e <jeje.e@kakaocorp.com>
2021-03-17 16:14:28 +0900

    init

---
+++ app/src/main/java/com/jeje/friendpicker/FriendPickerActivity.kt
@@ -0,0 +1,11 @@
+package com.jeje.friendpicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendPickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_picker)
+    }
+}
